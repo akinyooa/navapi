6fdb525d16b919e67092892b6cf00fbfacf3e051
Ade A <AdeAkinyooye@gmail.com>
2017-04-09 21:11:26 +0100

    Added post api for user profile and swagger integration

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # luna-api
+
+In order to use swagger run:
+
+*   npm install -g swagger
+*   start the server with node server
+*   browse to http://localhost:3010/dist/index.html
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
-var models = require('./lib/models/models');
-var userProfile = require('./lib/routes/userProfile');
+var port = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:63874");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/v1/', userProfile);
+var models = require('./lib/models/models');
+var userProfiles = require('./lib/routes/userProfiles');
 
-app.listen(3010, function() {
-    console.log('Server started on port 3010...');
+app.use('/dist', express.static(path.join(__dirname, 'public')));
+app.use('/api', express.static(path.join(__dirname, 'api')));
+app.use('/api/v1/', userProfiles);
+
+app.listen(port, function () {
+    console.log('Server started on port %s...', port);
 });
