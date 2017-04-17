9b279d60330cca9ef80e08a21250dfed3af65eca
Ade A <AdeAkinyooye@gmail.com>
2017-04-17 09:50:57 +0100

    Add startup for pm2

--- scripts/ubuntu/install-pm2.sh
+++ scripts/ubuntu/install-pm2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#https://www.digitalocean.com/community/tutorials/how-to-use-pm2-to-setup-a-node-js-production-environment-on-an-ubuntu-vps
 sudo npm install pm2 -g
+sudo env PATH=$PATH:/usr/local/bin pm2 startup -u ubuntu
