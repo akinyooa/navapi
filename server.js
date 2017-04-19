bfece302609b5e0071722127e9a0cefaefc63761
Ade A <AdeAkinyooye@gmail.com>
2017-04-19 22:59:34 +0100

    Add env variables for cassandra connection, update swagger.yaml

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var port = process.env.PORT || 80;
 
 var app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:63874");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://127.0.0.1:63874");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
