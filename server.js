6c12406fc8b45a29a508f8109db8fb021269b18c
Ade A <AdeAkinyooye@gmail.com>
2017-04-19 23:00:03 +0100

    update

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ var port = process.env.PORT || 80;
 
 var app = express();
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://127.0.0.1:63874");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
