5ca4bdb30d591206baefde05e634aac3384b843a
Ade A <AdeAkinyooye@gmail.com>
2017-04-22 04:15:13 +0100

    Load yaml based on env

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 In order to use swagger run:
 
-*   npm install -g swagger
-*   start the server with node server
-*   browse to http://localhost:3010/dist/index.html
+# Dev setup
+* npm install
+* start the server with node server
+
+# Swagger
+* npm install -g swagger
+* browse to http://localhost:3010/dist/index.html
+* to edit api/documentation run swagger project edit
--- server.js
+++ server.js
@@ -8,12 +8,19 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 var models = require('./lib/models/models');
 var userProfiles = require('./lib/routes/userProfiles');
 
-app.use('/dist', express.static(path.join(__dirname, 'public')));
-app.use('/api', express.static(path.join(__dirname, 'api')));
+app.use('/swagger', express.static(path.join(__dirname, 'public')));
+app.use('/swagger', express.static(path.join(__dirname, ('api/swagger'))));
 app.use('/api/v1/', userProfiles);
+
+app.get('/swagger', function (request, response) {
+    response.render(path.join(__dirname, '/public/index'), { yaml: process.env.NODE_ENV == "dev" ? "/swagger/swagger.dev.yaml" : "/swagger/swagger.yaml" });
+});
 
 app.listen(port, function () {
     console.log('Server started on port %s...', port);
